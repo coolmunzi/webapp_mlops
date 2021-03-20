34b2ea0f1e22373bbdcdf0c09b079acd2a20404d
Munjal Patel <munjal.patel6@gmail.com>
2021-03-20 17:49:58 +0530

    initial commit

---
+++ template.py
@@ -0,0 +1,27 @@
+import os
+
+dirs = [
+    os.path.join("data", "raw"),
+    os.path.join("data","processed"),
+    "data_given",
+    "notebooks",
+    "saved_models",
+    "src"
+]
+
+for dir in dirs:
+    os.makedirs(dir, exist_ok=True)
+    #Create .gitkeep file for commiting empty folder to git
+    with open(os.path.join(dir, ".gitkeep"), "w") as f:
+        pass
+
+files = [
+    "dvc.yaml",
+    "params.yaml",
+    ".gitignore",
+    os.path.join("src","__init__.py")
+]
+
+for file in files:
+    with open(file, "w") as f:
+        pass
